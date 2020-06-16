403fa832612ed62d6ab5ec57975f7b3c34c48f07
Rchambe9 <Rchambe9@gmail.com>
2020-06-15 22:33:50 -0400

    Refactor Tests

--- src/test/java/pets_amok/VirtualPetTest.java
+++ src/test/java/pets_amok/VirtualPetTest.java
@@ -1,24 +1,29 @@
 package pets_amok;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VirtualPetTest {
+    VirtualPet virtualPetTest;
+
+    @BeforeEach
+    public void setup(){
+        virtualPetTest = new VirtualPet("Scruffy");
+
+    }
 
     @Test
     public void shouldBeAbleToCreateVirtualPet(){
-        VirtualPet virtualPetTest = new VirtualPet("Scruffy");
 
         assertEquals("Scruffy", virtualPetTest.getName());
     }
 
     @Test
     public void hungerShouldBe8WhenCreated(){
-        VirtualPet virtualPetTest = new VirtualPet("Scruffy");
 
         assertEquals(8, virtualPetTest.getHunger());
-
 
     }
 
